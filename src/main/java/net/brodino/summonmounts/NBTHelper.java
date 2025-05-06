12c91d406d87585bb36f9e3f991ab749f3cdcf3a
Vincent Acanfora <brodino96@gmail.com>
2025-05-06 11:54:59 +0200

    chore: added logs

--- src/main/java/net/brodino/summonmounts/NBTHelper.java
+++ src/main/java/net/brodino/summonmounts/NBTHelper.java
@@ -5,25 +5,32 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.registry.Registry;
 
 public class NBTHelper {
 
     public static ItemStack saveMountData(Entity entity, ItemStack stack) {
         if (!(entity instanceof AbstractHorseEntity mount)) {
+            SummonMounts.LOGGER.info("Entity is not a Horse derivative");
             return stack;
         }
+
+        SummonMounts.LOGGER.info("Item nbt's before doing anything: {}", stack.getNbt().toString());
 
         NbtCompound stackNbt = new NbtCompound();
         NbtCompound mountNbt = new NbtCompound();
         mount.writeNbt(mountNbt);
+        SummonMounts.LOGGER.info("Created nbts");
 
         if (mountNbt.contains("ArmorItems")) {
+            SummonMounts.LOGGER.info("Mount armor is: {}", mountNbt.get("ArmorItems").toString());
             stackNbt.put("mount.armor", mountNbt.get("ArmorItems"));
             mountNbt.remove("ArmorItems");
         }
 
         if (mountNbt.contains("SaddleItem")) {
+            SummonMounts.LOGGER.info("Mount saddle is: {}", mountNbt.get("SaddleItem").toString());
             stackNbt.put("mount.saddle", mountNbt.getCompound("SaddleItem"));
             mountNbt.remove("SaddleItem");
         }
@@ -39,6 +46,8 @@ public class NBTHelper {
 
         stack.setNbt(stackNbt);
         stack.addEnchantment(Enchantments.LOYALTY, 1);
+
+        SummonMounts.LOGGER.info("Item nbt's after doing everything: {}", stack.getNbt().toString());
 
         return stack;
 
