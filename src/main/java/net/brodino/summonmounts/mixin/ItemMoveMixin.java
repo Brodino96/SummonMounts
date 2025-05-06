5065becc5b0e96dd17ba150e279e62e5d4729c5e
Vincent Acanfora <brodino96@gmail.com>
2025-05-06 11:54:33 +0200

    refactor: renamed mixin

--- src/main/java/net/brodino/summonmounts/mixin/ItemMoveMixin.java
+++ src/main/java/net/brodino/summonmounts/mixin/ItemMoveMixin.java
@@ -3,8 +3,6 @@ package net.brodino.summonmounts.mixin;
 import net.brodino.summonmounts.MountManager;
 import net.brodino.summonmounts.SummonMounts;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.BundleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.PlayerScreenHandler;
@@ -20,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.UUID;
 
 @Mixin(ScreenHandler.class)
-public class ItemMovementMixin {
+public class ItemMoveMixin {
 
     @Inject(method = "onSlotClick", at = @At("HEAD"), cancellable = true)
     private void onSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
