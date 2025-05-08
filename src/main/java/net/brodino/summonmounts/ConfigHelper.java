164e1abd6e98ab7052300368d50395dd4fe6f7cd
Brodino <brodino96@gmail.com>
2025-05-08 03:35:32 +0200

    fix: refactor allowedSummons to use ArrayList and initialize allowedDimensions

--- src/main/java/net/brodino/summonmounts/ConfigHelper.java
+++ src/main/java/net/brodino/summonmounts/ConfigHelper.java
@@ -2,16 +2,27 @@ package net.brodino.summonmounts;
 
 import io.wispforest.owo.config.annotation.Config;
 
+import java.util.ArrayList;
+
 @Config(name = "summonmounts", wrapperName = "Config")
 public class ConfigHelper {
 
     public String summonItem;
-    public String[] allowedSummons;
+    public ArrayList<String> allowedSummons;
     public Integer despawnTime;
+    public ArrayList<String> allowedDimensions;
 
     public ConfigHelper() {
-        this.summonItem = "minecraft:dragon_egg";
-        this.allowedSummons = new String[]{"minecraft:horse", "minecraft:mule", "minecraft:donkey"};
+        this.summonItem = "minecraft:echo_shard";
+
+        this.allowedSummons = new ArrayList<>();
+        this.allowedSummons.add("minecraft:horse");
+        this.allowedSummons.add("minecraft:mule");
+        this.allowedSummons.add("minecraft:donkey");
+
         this.despawnTime = 5;
+
+        this.allowedDimensions = new ArrayList<>();
+        this.allowedDimensions.add("minecraft:overworld");
     }
 }
