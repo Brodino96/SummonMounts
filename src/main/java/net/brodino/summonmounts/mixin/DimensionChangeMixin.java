ce2cb809685f98cb55050303976b5061637f54e8
Brodino <brodino96@gmail.com>
2025-05-08 03:36:15 +0200

    feat: add DimensionChangeMixin to handle player disconnection in ServerWorld

---
+++ src/main/java/net/brodino/summonmounts/mixin/DimensionChangeMixin.java
@@ -0,0 +1,24 @@
+package net.brodino.summonmounts.mixin;
+
+import net.brodino.summonmounts.MountManager;
+import net.brodino.summonmounts.SummonMounts;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerWorld.class)
+public class DimensionChangeMixin {
+
+    public DimensionChangeMixin() {
+        SummonMounts.LOGGER.info("DimensionChangeMixin");
+    }
+
+    @Inject(method = "removePlayer", at = @At("HEAD"), cancellable = false)
+    private void removePlayer(ServerPlayerEntity player, Entity.RemovalReason reason, CallbackInfo ci) {
+        MountManager.playerDisconnected(player);
+    }
+}
