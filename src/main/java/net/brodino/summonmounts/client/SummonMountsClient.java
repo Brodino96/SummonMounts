aa7615f8b9eede6ee1a2faaa9fd385de4e1579b7
Brodino <brodino96@gmail.com>
2026-08-27 20:55:17 +0200

    feat(client): added client entrypoint

---
+++ src/main/java/net/brodino/summonmounts/client/SummonMountsClient.java
@@ -0,0 +1,10 @@
+package net.brodino.summonmounts.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SummonMountsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
