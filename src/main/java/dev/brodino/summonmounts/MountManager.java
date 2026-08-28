141e88b5aa476a9c9bff1d28a143dcb6edd2e9b4
Brodino <brodino96@gmail.com>
2026-08-28 15:45:47 +0200

    feat(manager): added basic methods

--- src/main/java/dev/brodino/summonmounts/MountManager.java
+++ src/main/java/dev/brodino/summonmounts/MountManager.java
@@ -1,13 +1,43 @@
 package dev.brodino.summonmounts;
 
 import dev.brodino.summonmounts.mount.Mount;
+import net.minecraft.entity.player.PlayerEntity;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.UUID;
 
 public class MountManager {
 
     private static final HashMap<UUID, Mount> MOUNTS = new HashMap<>();
 
+    public static void summon(PlayerEntity player, Mount mount) {
+        mount.summon();
+        MOUNTS.put(player.getUuid(), mount);
+    }
 
+    public static void recall(PlayerEntity player) {
+        getActiveMount(player).recall();
+        MOUNTS.remove(player.getUuid());
+    }
+
+    public static boolean hasActiveMount(PlayerEntity player) {
+        return MOUNTS.containsKey(player.getUuid());
+    }
+
+    public static Mount getActiveMount(PlayerEntity player) {
+        return MOUNTS.get(player.getUuid());
+    }
+
+    public static void tick() {
+        Iterator<Map.Entry<UUID, Mount>> iterator = MOUNTS.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<UUID, Mount> entry = iterator.next();
+
+            // Tick every mount
+            iterator.remove();
+        }
+    }
 }
