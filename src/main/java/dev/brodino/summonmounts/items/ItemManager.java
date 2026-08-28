743d4319a86b13dd336505194adfe581ee182f24
Brodino <brodino96@gmail.com>
2026-08-28 17:11:25 +0200

    feat(items): reworked manager class, added isItem check

--- src/main/java/dev/brodino/summonmounts/items/ItemManager.java
+++ src/main/java/dev/brodino/summonmounts/items/ItemManager.java
@@ -2,35 +2,45 @@ package dev.brodino.summonmounts.items;
 
 import dev.brodino.summonmounts.SummonMounts;
 import dev.brodino.summonmounts.items.flutes.*;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 public class ItemManager {
 
+    public static final FluteItem COPPER_FLUTE = register(FluteTypes.COPPER.toString(), new CopperFlute());
+    public static final FluteItem IRON_FLUTE = register(FluteTypes.IRON.toString(), new IronFlute());
+    public static final FluteItem GOLD_FLUTE = register(FluteTypes.GOLD.toString(), new GoldFlute());
+    public static final FluteItem DIAMOND_FLUTE = register(FluteTypes.DIAMOND.toString(), new DiamondFlute());
+    public static final FluteItem EMERALD_FLUTE = register(FluteTypes.EMERALD.toString(), new EmeraldFlute());
 
-    private static final HashMap<FluteTypes, FluteItem> ITEMS = new HashMap<>(){{
-        put(FluteTypes.COPPER, new CopperFlute());
-        put(FluteTypes.IRON, new IronFlute());
-        put(FluteTypes.GOLD, new GoldFlute());
-        put(FluteTypes.DIAMOND, new DiamondFlute());
-        put(FluteTypes.EMERALD, new EmeraldFlute());
-    }};
 
     public static void initialize() {
-        for (Map.Entry<FluteTypes, FluteItem> entry : ITEMS.entrySet()) {
-            register(entry.getKey().toString(), entry.getValue());
-        }
+        SummonMounts.LOGGER.info("Initializing flutes");
     }
 
-    private static void register(String id, FluteItem flute) {
-        Registry.register(Registry.ITEM, new Identifier(SummonMounts.MOD_ID, id.toLowerCase(Locale.ROOT) + "_flute"), flute);
+    private static FluteItem register(String id, FluteItem flute) {
+        return Registry.register(Registry.ITEM, new Identifier(SummonMounts.MOD_ID, id.toLowerCase(Locale.ROOT) + "_flute"), flute);
     }
 
     public static FluteItem getItemFromEnum(FluteTypes item) {
-        return ITEMS.get(item);
+        return switch (item) {
+            case COPPER -> COPPER_FLUTE;
+            case IRON -> IRON_FLUTE;
+            case GOLD -> GOLD_FLUTE;
+            case DIAMOND -> DIAMOND_FLUTE;
+            case EMERALD -> EMERALD_FLUTE;
+        };
+    }
+
+    public static boolean isFlute(Item item) {
+        return item != null
+                && ( item.equals(COPPER_FLUTE)
+                || item.equals(IRON_FLUTE)
+                || item.equals(GOLD_FLUTE)
+                || item.equals(DIAMOND_FLUTE)
+                || item.equals(EMERALD_FLUTE) );
     }
 }
