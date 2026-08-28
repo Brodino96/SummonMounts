1641b97940a4c261dbbb73affb346e9f3aaed563
Brodino <brodino96@gmail.com>
2026-08-28 04:20:52 +0200

    feat(save): implemented save process

--- src/main/java/dev/brodino/summonmounts/items/flutes/FluteItem.java
+++ src/main/java/dev/brodino/summonmounts/items/flutes/FluteItem.java
@@ -1,7 +1,12 @@
 package dev.brodino.summonmounts.items.flutes;
 
+import dev.brodino.summonmounts.SummonMounts;
+import dev.brodino.summonmounts.mount.Mount;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 
 public class FluteItem extends Item {
 
@@ -13,5 +18,34 @@ public class FluteItem extends Item {
     public FluteItem(Settings settings) {
         super(settings);
     }
+
+    // Nbt stuff
+
+    /**
+     * Checks if the {@link NbtCompound} is linked to a {@link Mount}
+     * @param nbt The {@link NbtCompound} to check
+     * @return true if it finds data linked to a {@link Mount}
+     */
+    public static boolean containsMount(NbtCompound nbt) {
+        return nbt != null && nbt.contains(SummonMounts.MOD_ID);
+    }
+
+    public static NbtCompound getMountData(ItemStack stack) {
+        final NbtCompound nbt = stack.getNbt();
+        return !containsMount(nbt) ? null : nbt.getCompound(SummonMounts.MOD_ID);
+    }
+
+    /**
+     * Saves the {@link Mount} inside the {@link FluteItem}
+     * @param stack {@link ItemStack} of the {@link FluteItem}
+     * @param mount The {@link Mount}
+     */
+    public static void saveMount(ItemStack stack, Mount mount) {
+        NbtCompound mountData = mount.getSavableNbt();
+        stack.getOrCreateNbt().put(SummonMounts.MOD_ID, mountData);
+        stack.addEnchantment(Enchantments.LOYALTY, 1);
+        // ItemLore
+    }
+
 
 }
--- src/main/java/dev/brodino/summonmounts/mount/Mount.java
+++ src/main/java/dev/brodino/summonmounts/mount/Mount.java
@@ -1,12 +1,19 @@
 package dev.brodino.summonmounts.mount;
 
+import dev.brodino.summonmounts.items.flutes.FluteItem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+
+import java.util.UUID;
 
 @Environment(EnvType.SERVER)
 public class Mount implements ParticleHelper {
@@ -14,20 +21,22 @@ public class Mount implements ParticleHelper {
     private final ServerPlayerEntity summoner;
     private final AbstractHorseEntity entity;
     private final ItemStack stack;
+    private final Identifier id;
 
     private Mount(ServerPlayerEntity summoner, AbstractHorseEntity entity, ItemStack stack) {
         this.summoner = summoner;
         this.entity = entity;
         this.stack = stack;
+        this.id = Registry.ENTITY_TYPE.getId(this.entity.getType());
     }
 
-    public static Mount fromEntity(ServerPlayerEntity summoner, AbstractHorseEntity entity, ItemStack stack) {
+    public static void fromEntity(ServerPlayerEntity summoner, AbstractHorseEntity entity, ItemStack stack) {
         Mount mount = new Mount(summoner, entity, stack);
         mount.recall();
-        return mount;
     }
 
     public void recall() {
+        FluteItem.saveMount(this.stack, this);
         // Save data inside itemstack
         this.entity.discard();
         this.recallParticles((ServerWorld) this.entity.getWorld(), this);
@@ -43,4 +52,28 @@ public class Mount implements ParticleHelper {
     public Vec3d getPos() { return this.entity.getPos(); }
     public double getHeight() { return this.entity.getHeight(); }
     public double getRadius() { return this.entity.getBoundingBox().getAverageSideLength(); }
+
+    public NbtCompound getSavableNbt() {
+        NbtCompound nbt = new NbtCompound();
+        NbtCompound mountNbt = new NbtCompound();
+        entity.writeNbt(mountNbt);
+
+        if (!entity.isDead()) {
+            if (mountNbt.contains("ArmorItems")) { nbt.put("armor", mountNbt.get("ArmorItems")); }
+            if (mountNbt.contains("SaddleItem")) { nbt.put("saddle", mountNbt.get("SaddleItem")); }
+            if (mountNbt.contains("DecorItem")) { nbt.put("decor", mountNbt.get("DecorItem")); }
+        }
+
+        Text name = entity.getDisplayName();
+        if (name != null) { nbt.putString("name", name.getString()); }
+
+        UUID owner = entity.getOwnerUuid();
+        if (owner != null) { nbt.putUuid("owner", owner); }
+
+        if (mountNbt.contains("Variant")) { nbt.putInt("variant", mountNbt.getInt("Variant")); }
+
+        nbt.putString("type", this.id.toString());
+
+        return nbt;
+    }
 }
