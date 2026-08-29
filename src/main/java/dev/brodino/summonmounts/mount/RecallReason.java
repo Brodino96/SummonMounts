9869e1b3d27421ab6a6b0b774c7becd4afabcd26
Brodino <brodino96@gmail.com>
2026-08-29 18:27:57 +0200

    chore(recall): changed declaration to directly hold log

--- src/main/java/dev/brodino/summonmounts/mount/RecallReason.java
+++ src/main/java/dev/brodino/summonmounts/mount/RecallReason.java
@@ -1,29 +1,29 @@
 package dev.brodino.summonmounts.mount;
 
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+
 import java.util.Locale;
 
 public enum RecallReason {
-    NONE,
-    IDLE,
-    ALIVE,
-    DEATH,
-    MANUAL,
-    AIRBORNE,
-    DIMENSION,
-    DISCONNECT;
+    NONE("Recalling {}'s mount"),
+    IDLE("Recalling {}'s mount because it was idle for too long"),
+    TAME("Recalling {}'s mount because it just got tame"),
+    ALIVE("Recalling {}'s mount because it was alive for too long"),
+    DEATH("Recalling {}'s mount because it died"),
+    MANUAL("Recalling {}'s mount because of manual recall"),
+    AIRBORNE("Recalling {}'s mount because it's stayed airborne for too long"),
+    DIMENSION("Recalling {}'s mount because player changed dimension"),
+    DISCONNECT("Recalling {}'s mount because player disconnected");
 
-    public String reason() { return "recall.summonmounts.feedback." + this.toString().toLowerCase(Locale.ROOT); }
+    private final String log;
+    private final MutableText reason;
 
-    public static String getLog(RecallReason reason) {
-        return switch (reason) {
-            case IDLE -> "Recalling {}'s mount because it was idle for too long";
-            case ALIVE -> "Recalling {}'s mount because it was alive for too long";
-            case DEATH -> "Recalling {}'s mount because it died";
-            case MANUAL -> "Recalling {}'s mount because of manual recall";
-            case AIRBORNE -> "Recalling {}'s mount because it's stayed airborne for too long";
-            case DIMENSION -> "Recalling {}'s mount because player changed dimension";
-            case DISCONNECT -> "Recalling {}'s mount because player disconnected";
-            default -> "Recalling {}'s mount";
-        };
+    RecallReason(String log) {
+        this.log = log;
+        this.reason = Text.translatable("recall.summonmounts.feedback." + this.toString().toLowerCase(Locale.ROOT));
     }
+
+    public String getLog() { return this.log; }
+    public MutableText getReason() { return this.reason; }
 }
