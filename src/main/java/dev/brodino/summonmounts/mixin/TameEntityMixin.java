3d8d674f6265a261e25bd8f255339711b6a7c493
Brodino <brodino96@gmail.com>
2026-08-29 17:41:49 +0200

    chore(mixin): removed unused shadow

--- src/main/java/dev/brodino/summonmounts/mixin/TameEntityMixin.java
+++ src/main/java/dev/brodino/summonmounts/mixin/TameEntityMixin.java
@@ -9,11 +9,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.StackReference;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -22,9 +20,6 @@ import java.util.Optional;
 
 @Mixin(AbstractHorseEntity.class)
 public abstract class TameEntityMixin {
-
-    @Shadow
-    public abstract StackReference getStackReference(int mappedIndex);
 
     @Environment(EnvType.SERVER)
     @Inject(method = "bondWithPlayer", at = @At("TAIL"))
