3161ab1c20f4ed6ee1f728f4af5566a9e1e1c1e1
Brodino <brodino96@gmail.com>
2026-08-29 17:42:40 +0200

    chore(events): removed unused event

--- src/main/java/dev/brodino/summonmounts/EventHandlers.java
+++ src/main/java/dev/brodino/summonmounts/EventHandlers.java
@@ -4,9 +4,7 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.util.ActionResult;
 
 public class EventHandlers {
 
@@ -26,13 +24,6 @@ public class EventHandlers {
         // Register entity death event - use ALLOW_DEATH to intercept before items drop
         SummonMounts.LOGGER.info("Registering death event");
         ServerLivingEntityEvents.ALLOW_DEATH.register(MountManager::onMountDeath);
-
-        // Register item use on entity callbacks
-        SummonMounts.LOGGER.info("Registering itemUseOnEntity event");
-        UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
-            MountManagerOld.itemUsedOnAnEntity(player, world, hand, entity, hitResult);
-            return ActionResult.PASS;
-        });
 
         ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register(MountManager::onDimensionChange);
     }
