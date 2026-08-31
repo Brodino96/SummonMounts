3590c9c9a3203fa7741012225b2502f2c674f7a0
Brodino <brodino96@gmail.com>
2026-08-31 21:35:39 +0200

    feat(config): added default config entries implementing mythicmounts characters

--- src/main/java/dev/brodino/summonmounts/config/ConfigType.java
+++ src/main/java/dev/brodino/summonmounts/config/ConfigType.java
@@ -22,7 +22,26 @@ public class ConfigType {
     }};
     public List<MountEntry> mounts = List.of(
             new MountEntry("minecraft:donkey", OcarinaTypes.INFERIOR),
+
             new MountEntry("minecraft:mule", OcarinaTypes.LESSER),
-            new MountEntry("minecraft:horse", OcarinaTypes.MEDIUM)
+            new MountEntry("minecraft:llama", OcarinaTypes.LESSER),
+
+            new MountEntry("minecraft:horse", OcarinaTypes.MEDIUM),
+
+            new MountEntry("mythicmounts:acencia", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:archelon", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:courierbird", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:direwolf", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:geckotoalizard", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:nightmare", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:nudibranch", OcarinaTypes.GREATER),
+            new MountEntry("mythicmounts:ridinglizard", OcarinaTypes.GREATER),
+
+            new MountEntry("mythicmounts:colelytra", OcarinaTypes.SUPERIOR),
+            new MountEntry("mythicmounts:dragon", OcarinaTypes.SUPERIOR),
+            new MountEntry("mythicmounts:firebird", OcarinaTypes.SUPERIOR),
+            new MountEntry("mythicmounts:griffon", OcarinaTypes.SUPERIOR),
+            new MountEntry("mythicmounts:moth", OcarinaTypes.SUPERIOR),
+            new MountEntry("mythicmounts:netherbat", OcarinaTypes.SUPERIOR)
     );
 }
