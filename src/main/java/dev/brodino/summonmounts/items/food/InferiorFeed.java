ef1d1b7d586c001e7fc95b97bfbab566ea122dc7
Brodino <brodino96@gmail.com>
2026-08-31 14:51:27 +0200

    chore(imports): optimized imports

--- src/main/java/dev/brodino/summonmounts/items/food/InferiorFeed.java
+++ src/main/java/dev/brodino/summonmounts/items/food/InferiorFeed.java
@@ -1,7 +1,7 @@
 package dev.brodino.summonmounts.items.food;
 
-import dev.brodino.summonmounts.items.OcarinaTypes;
 import dev.brodino.summonmounts.items.FeedItem;
+import dev.brodino.summonmounts.items.OcarinaTypes;
 import net.minecraft.util.Rarity;
 
 public class InferiorFeed extends FeedItem {
